b7aafe6f51b7d5749fad3b39708c8653a2c65710
Sabrina Moczulski <sabrina@belasis.com.br>
2017-11-21 19:23:31 -0200

    Add html, css

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
 
+
 	def index
 		@posts = Post.all.order('created_at DESC')
 	end
@@ -16,6 +17,11 @@ class PostsController < ApplicationController
 
 	def show
 		@post = Post.find(params[:id])
+	end
+
+	def edit
+		@post = Post.find(params[:id])
+		
 	end
 
 	private
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   
   resources :posts
   root "posts#index"
+  # get "posts/edit/:id" => "posts#edit"
+  # post "posts/:id" => "posts#update"
 end
+
